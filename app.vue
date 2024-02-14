155f9053e9659f3e75a181ab577ff33e9e46b301
Aung Thu Zaw ( Xyder ) <105932994+aung-thu-zaw@users.noreply.github.com>
2024-02-14 13:36:56 +0630

    install nuxt and tailwind module

---
+++ app.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <!-- <NuxtWelcome /> -->
+    <div class="font-bold text-lg bg-red-200 text-red-600 text-center w-full">
+      Hello World
+    </div>
+  </div>
+</template>
