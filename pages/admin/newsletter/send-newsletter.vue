310fed4e35a63ce55771816fa8a06d6140d853fb
Aung Thu Zaw ( Xyder ) <105932994+aung-thu-zaw@users.noreply.github.com>
2024-02-25 11:40:05 +0630

    implement send newsletter

--- pages/admin/newsletter/send-newsletter.vue
+++ pages/admin/newsletter/send-newsletter.vue
@@ -1,64 +1,62 @@
 <script setup lang="ts">
 import Breadcrumb from '~/components/Breadcrumbs/MainBreadcrumb.vue'
 import BreadcrumbItem from '~/components/Breadcrumbs/BreadcrumbItem.vue'
-import BreadcrumbLinkItem from '~/components/Breadcrumbs/BreadcrumbLinkItem.vue'
 import InputLabel from '~/components/Forms/Fields/InputLabel.vue'
 import InputError from '~/components/Forms/Fields/InputError.vue'
 import InputField from '~/components/Forms/Fields/InputField.vue'
-import FileInput from '~/components/Forms/Fields/FileInput.vue'
-import SelectBox from '~/components/Forms/Fields/SelectBox.vue'
 import FormButton from '~/components/Buttons/FormButton.vue'
 import GoBackButton from '~/components/Buttons/GoBackButton.vue'
-import PreviewImageBox from '~/components/Forms/PreviewImageBox.vue'
-import { useBlogContentStore } from '~/stores/dashboard/admin/blogContent'
-import { storeToRefs } from 'pinia'
-import type { Form } from '~/types/blogContent'
-import { useImagePreview } from '@/composables/useImagePreview'
 import { QuillEditor } from '@vueup/vue-quill'
 import '@vueup/vue-quill/dist/vue-quill.snow.css'
-import image from '@/assets/images/no-image.jpeg'
+import { useToken } from '~/composables/useToken'
 
-useHead({ title: 'Create Blog Content' })
+useHead({ title: 'Send Newsletter' })
 
 definePageMeta({ layout: 'admin-layout' })
 
-const store = useBlogContentStore()
-const isCreateAnother = ref<boolean>(false)
-const createForm = ref<HTMLFormElement | null>(null)
-const { errors, blogCategories } = storeToRefs(store)
-const { previewImage, setImagePreview } = useImagePreview()
-const form: Form = reactive({
-  blog_category_id: '',
-  title: '',
-  status: '',
-  content: '',
-  thumbnail: ''
-})
-
-const handleCreateBlogContent = async () => {
-  await store.createBlogContent({ ...form }, isCreateAnother.value)
-  if (isCreateAnother.value && !store.errors) {
-    console.log(typeof createForm.value)
-    createForm?.value?.reset()
-    previewImage.value = image
-    form.blog_category_id = ''
-    form.title = ''
-    form.status = ''
-    form.content = ''
-    form.thumbnail = ''
-  }
+interface Form {
+  subject: string
+  content: string
+  captcha_token: string | null
 }
 
-onMounted(async () => await store.getResources())
+const { $axiosApi, $toast } = useNuxtApp()
+const { generateCaptchaToken } = useToken()
+const errors = ref<any>(null)
+const newsletterForm = ref<HTMLFormElement | null>(null)
+const form: Form = reactive({
+  subject: '',
+  content: '',
+  captcha_token: null
+})
+
+const handleSendNewsletter = async () => {
+  try {
+    form.captcha_token = await generateCaptchaToken('send_newsletter')
+
+    const response = await $axiosApi.post('/admin/send-newsletter', { ...form })
+
+    if (!response) throw new Error('Response Not Found!')
+
+    newsletterForm?.value?.reset()
+
+    $toast.success(response?.data?.message)
+  } catch (error) {
+    errors.value = error.response?.data?.errors
+  }
+}
 </script>
 
 <template>
   <div class="h-auto space-y-5">
     <!-- Breadcrumb -->
     <div class="flex flex-col items-start md:flex-row md:items-center md:justify-between mb-10">
-      <Breadcrumb to="/admin/manage-blog/contents" icon="fa-file-pen" label="Manage Blog">
-        <BreadcrumbLinkItem to="/admin/manage-blog/contents" label="Contents" />
-        <BreadcrumbItem label="Create" />
+      <Breadcrumb
+        to="/admin/newsletter/send-subscribers"
+        icon="fa-envelope-open-text"
+        label="Newsletter"
+      >
+        <BreadcrumbItem label="Send" />
       </Breadcrumb>
 
       <!-- Go Back  -->
@@ -70,24 +68,22 @@ onMounted(async () => await store.getResources())
     <!-- Form Start -->
     <div class="border p-10 bg-white rounded-md">
       <form
-        ref="createForm"
+        ref="newsletterForm"
         class="space-y-4 md:space-y-6"
-        @submit.prevent="handleCreateBlogContent"
+        @submit.prevent="handleSendNewsletter"
       >
-        <PreviewImageBox :src="previewImage" />
-
         <div>
-          <InputLabel label="Title" required />
+          <InputLabel label="Subject" required />
 
           <InputField
-            v-model="form.title"
+            v-model="form.subject"
             type="text"
-            name="content-title"
-            placeholder="Enter Blog Title"
+            name="subject"
+            placeholder="Enter Subject"
             required
           />
 
-          <InputError :message="errors?.title" />
+          <InputError :message="errors?.subject" />
         </div>
 
         <div>
@@ -114,77 +110,12 @@ onMounted(async () => await store.getResources())
           <InputError :message="errors?.content" />
         </div>
 
-        <div class="grid grid-cols-1 md:grid-cols-2 gap-5">
-          <div>
-            <InputLabel label="Status" required />
-
-            <SelectBox
-              v-model="form.status"
-              name="category-status"
-              :options="[
-                {
-                  label: 'Draft',
-                  value: 'draft'
-                },
-                {
-                  label: 'Published',
-                  value: 'published'
-                },
-                {
-                  label: 'Hidden',
-                  value: 'hidden'
-                }
-              ]"
-              required
-            />
-
-            <InputError :message="errors?.status" />
-          </div>
-
-          <div>
-            <InputLabel label="Category" required />
-
-            <SelectBox
-              v-model="form.blog_category_id"
-              name="blog-category"
-              :options="blogCategories"
-              required
-              :selected="form.blog_category_id === null"
-            />
-
-            <InputError :message="errors?.blog_category_id" />
-          </div>
-        </div>
-
-        <div>
-          <InputLabel label="Thumbnail" required />
-
-          <FileInput
-            v-model="form.thumbnail"
-            name="blog-content-thumbnail"
-            text="PNG, JPG or JPEG ( Max File Size : 1.5 MB )"
-            required
-            @update:model-value="setImagePreview"
-          />
-
-          <InputError :message="store.errors?.thumbnail" />
-        </div>
-
         <InputError :message="errors?.captcha_token" />
 
         <div class="flex items-center justify-end w-full space-x-5">
-          <FormButton
-            class="w-[250px] text-white bg-gray-600 hover:bg-gray-700 rounded-md"
-            @click="isCreateAnother = true"
-          >
-            Save And Create Another
-          </FormButton>
-
-          <FormButton
-            class="w-[100px] text-white bg-yellow-600 hover:bg-yellow-700 rounded-md"
-            @click="isCreateAnother = false"
-          >
-            Save
+          <FormButton class="text-white bg-yellow-600 hover:bg-yellow-700 rounded-md w-full">
+            <i class="fa-solid fa-paper-plane mr-2"></i>
+            Send Newsletter
           </FormButton>
         </div>
       </form>
